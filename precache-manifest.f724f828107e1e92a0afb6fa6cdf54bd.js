46d4648d4985e3ac835efcbfc63206162163bec1
Sneh Koul <snehkoul1999@gmail.com>
2019-03-05 15:42:06 -0800

    Updates

---
+++ precache-manifest.f724f828107e1e92a0afb6fa6cdf54bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a313811ff70c171ad57756e1039182c",
+    "url": "/Button-MetaMask/static/media/Ropsten.7a313811.png"
+  },
+  {
+    "revision": "2f8fbd099e95b7ad53dc",
+    "url": "/Button-MetaMask/static/js/runtime~main.2f8fbd09.js"
+  },
+  {
+    "revision": "be1cb396167174d86103",
+    "url": "/Button-MetaMask/static/js/main.be1cb396.chunk.js"
+  },
+  {
+    "revision": "de1f9accddfde3578f20",
+    "url": "/Button-MetaMask/static/js/1.de1f9acc.chunk.js"
+  },
+  {
+    "revision": "d09b1391c50610935a518a69416144ee",
+    "url": "/Button-MetaMask/index.html"
+  }
+];
